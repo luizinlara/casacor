868f737b43525b5da648c25b2edad317e5e238cd
ykaruus <147627870+ykaruus@users.noreply.github.com>
2023-10-24 11:35:29 -0400

    Menu slider adicionado
    Menu slider adicionado

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
-const $menu = document.getElementById('menu');
-const $btn = document.getElementById('btn');
-const $btn_close = document.getElementById('btnClose')
-const $toggle_class = document.querySelector('.menu-item')
-const $menu_background = document.querySelector(`.menu-background`)
-let state = false
+const $menu = document.querySelector('.caixa');
+const $menu_background = document.querySelector('.menu-background');
+const $menuIcon = $menu_background.querySelector('.menu-icon')
+const $open_icon = $menuIcon.querySelector('.open-icon');
+const $close_icon = $menuIcon.querySelector('.close-icon');
 
+$menu.classList.add('caixa-close')
 
-$btn.addEventListener('click', () => {
-    if($menu.style.left = '0px') {
-        $menu.style.left = '-450px'
-        $menu_background.style.left = '0px'
-        $btn.classList.add('girar-icone') /**Gira o icone */
-    } else {
-        $menu.style.left = '-450px';
-        $menu_background.style.left = '300px'
-        $btn.classList.remove('girar-icone') /**remove o Giro o icone */
+$menuIcon.addEventListener('click', () => {
+    if($menu.className == 'caixa caixa-close') {
+        $menu.classList.add('caixa-open')
+        $menu_background.style.left = '300px';
+        $menuIcon.classList.toggle('close-icon')
     }
-})
+    else {
+        $menu.classList.remove('caixa-open')
+        $menu_background.style.left = '0px';
+        $menuIcon.classList.remove('close-icon')
+    }
+
+});
